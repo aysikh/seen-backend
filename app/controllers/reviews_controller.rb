62a2dc372c2b67661d854629dc630d4c361fe5f1
aysikh <amy.sikhammountry@gmail.com>
2021-02-25 17:24:21 -0500

    created crud functions for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,80 @@
 class ReviewsController < ApplicationController
+  before_action :find_review, only: [:show, :edit]
+
+  def index
+    @reviews = Review.all
+    render json: @reviews
+  end
+
+  def show
+  end
+
+  def new
+    @review = Review.create
+  end
+
+  def create
+    @review = Review.create(review_params)
+    @user= User.find(:user_id)
+    @company = Company.find(:company_id)
+    if @review.save
+      render json: {
+        review: @review, 
+        errors: false
+      }
+    else 
+      render json: {
+        errors: true,
+        info: ["review cannot be created"]
+      }
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review
+      @review.update(review_params)
+      render json: {
+        review: @review,
+        errors: false
+      }
+    else
+      render json: {
+        errors: true, 
+        info: ["review cannot be updated"]
+      }
+    end
+  end
+
+  def destroy
+    @reviews = Review.all
+    @review = find_review
+    if @review.destroy 
+      render json: {
+        review: @reviews, 
+        errors: "review has been deleted",
+        success: true
+      }
+    else
+      render json: {
+        success: false, 
+        errors: false, 
+        info: ["cannot delete review"]
+      }
+    end
+  end
+
+  private
+
+  def find_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:title, :date, :position, :pros, :cons, :rating, :isCurrentlyEmployed, :yearsEmployed, :isUseful, :isNotUseful, :user_id, :company_id)
+  end
+
 end
