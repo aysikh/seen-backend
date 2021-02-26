54b99ded7f1339710f02b6a8523552ae01621ab1
aysikh <amy.sikhammountry@gmail.com>
2021-02-25 19:04:46 -0500

    created CRUD functions for companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,77 @@
 class CompaniesController < ApplicationController
+  before_action :find_company, only: [:show, :edit]
+
+  def index
+    @companies = Company.all
+    render json: @companies
+  end
+
+  def show
+  end
+
+  def new 
+    @company = Company.create
+  end
+
+  def create
+    @company = Company.create(company_params)
+    if @company.save
+      render json: {
+        company: @company,
+        errors: false
+      }
+    else
+      render json: {
+        errors:true, 
+        info: ["company cannot be created"]
+      }
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    if @company
+      @company.update(company_params)
+      render json: {
+        company: @company, 
+        errors: false
+      }
+    else
+      render json: {
+        errors: true, 
+        info: ["company cannot be updated"]
+      }
+    end
+  end
+
+  def destroy 
+    @company = find_company
+    if @company.destroy
+      render json: {
+        company: @company, 
+        errors: "company has been removed", 
+        success: true
+      }
+    else
+      render json: {
+        success: false, 
+        errors: false, 
+        info: ["cannot delete company"]
+      }
+    end
+  end
+
+  private
+
+  def find_company
+    @company = Company.find(params[:id])
+  end
+
+  def company_params
+    params.require(:company).permit(:name, :description, :location, :size, :industry, :rating)
+  end
+
 end
